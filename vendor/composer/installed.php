7f0b6be1ed9472fb44c962fe6461dd57e70d018f
BOUDOUAH-Ilias <ilias.boudouah8@etu.univ-lorraine.fr>
2025-11-09 16:12:50 +0100

    composertrucjsp

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '792d7f6585bd2fc7ee802a378258bb05ec27afca',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '792d7f6585bd2fc7ee802a378258bb05ec27afca',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
